5c0f7ebfef47d95ee0675ec54558c008f958e924
miktokki <mikktok@gmail.com>
2026-03-28 09:41:10 +0200

    Bootstrap lisätty

--- src/main/java/project/cinema/web/CinemaController.java
+++ src/main/java/project/cinema/web/CinemaController.java
@@ -2,7 +2,6 @@ package project.cinema.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import ch.qos.logback.core.model.Model;
 
