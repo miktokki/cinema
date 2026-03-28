94557467323a30ff4f3b3d6bcfcb0dd438dd74a5
miktokki <mikktok@gmail.com>
2026-03-28 09:13:34 +0200

    Harjoitustyö aloitettu (dependencyt, controller, index.html)

---
+++ src/main/java/project/cinema/web/CinemaController.java
@@ -0,0 +1,17 @@
+package project.cinema.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import ch.qos.logback.core.model.Model;
+
+@Controller
+public class CinemaController {
+
+    @GetMapping("/testi")
+    public String teeTesti(Model model) {
+        return "index";
+    }
+
+}
